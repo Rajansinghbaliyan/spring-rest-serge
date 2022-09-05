46b1e48f2c8926d5465accb11a212a50dce1012a
RajanBaliyan <rajansinghbaliyan@gmail.com>
2022-09-06 00:43:15 +0530

    fix: added the optional to find by email

--- src/main/java/io/cherrytechnologies/springrestserge/io/repository/UserRepository.java
+++ src/main/java/io/cherrytechnologies/springrestserge/io/repository/UserRepository.java
@@ -4,9 +4,10 @@ import io.cherrytechnologies.springrestserge.io.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-    UserEntity findByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
