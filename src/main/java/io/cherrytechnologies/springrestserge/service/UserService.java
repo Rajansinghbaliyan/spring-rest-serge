41a05fd3e0239a8e1ac6b198ef26defc7be10be4
RajanBaliyan <rajansinghbaliyan@gmail.com>
2022-09-06 16:45:06 +0530

    feat: removed some left over from spring security

--- src/main/java/io/cherrytechnologies/springrestserge/service/UserService.java
+++ src/main/java/io/cherrytechnologies/springrestserge/service/UserService.java
@@ -1,13 +1,12 @@
 package io.cherrytechnologies.springrestserge.service;
 
 import io.cherrytechnologies.springrestserge.shared.dto.UserDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public interface UserService extends UserDetailsService {
+public interface UserService{
     public UserDto getUserById(UUID id);
     public UserDto saveUser(UserDto dto);
 }
